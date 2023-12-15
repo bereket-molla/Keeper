7fa5757654a06e876748eae37bf225c711f9e3fd
bereket-molla <bereketmolla@Berekets-MacBook-Air.local>
2023-12-15 14:40:34 -0500

    changed app to support backend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppHeader from "./Header";
 import AppFooter from "./Footer";
 import AppNote from "./Note";
@@ -7,13 +6,32 @@ import Keeper from "./Keeper";
 
 const App = () => {
   const [noteStack, setNoteStack] = useState([]);
+  const URL =
+    "https://keeper-backend-production-6c3a.up.railway.app/api/notes/";
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setNoteStack(data))
+      .catch((error) => console.error("Error fetching notes:", error));
+  }, []);
 
-  const handleNewNote = (noteContent) => {
-    setNoteStack(noteStack.concat(noteContent));
+  const handleNewNote = async (noteContent) => {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(noteContent)
+    });
+    const newNote = await response.json();
+    setNoteStack(noteStack.concat(newNote));
   };
 
-  const handleNoteDeletion = (noteIndex) => {
-    setNoteStack(noteStack.filter((_, idx) => idx !== noteIndex));
+  const handleNoteDeletion = async (noteId) => {
+    await fetch(URL + `?id=${noteId}`, {
+      method: "DELETE"
+    });
+    setNoteStack(noteStack.filter((note) => note._id !== noteId));
   };
 
   return (
@@ -22,8 +40,8 @@ const App = () => {
       <Keeper onEntrySubmit={handleNewNote} />
       {noteStack.map((singleNote, idx) => (
         <AppNote
-          key={idx}
-          id={idx}
+          key={singleNote._id}
+          id={singleNote._id}
           title={singleNote.title}
           content={singleNote.body}
           onDelete={handleNoteDeletion}
